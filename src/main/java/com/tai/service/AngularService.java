1e5f1ffe53842d80e85bbf290b7312c54a95b443
gulnurdemircioglu <gulnurrd@gmail.com>
2019-05-13 16:22:44 +0300

    first commit

---
+++ src/main/java/com/tai/service/AngularService.java
@@ -0,0 +1,58 @@
+package com.tai.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Stateless
+public class AngularService extends MockService {
+
+	static List<MockService> mockServiceList = new ArrayList();	
+	
+	@GET
+	@Path("/mock_service/{path}")
+	public Response getMockServerResponseGet(@PathParam(value = "path") String path) { 
+		for(MockService mockService : mockServiceList) {
+			if(mockService.getUrl().equals(path) && 
+					"GET".equals(mockService.getHttpMethod())) {
+				
+				return Response.ok(mockService.getResponse()).build();
+			}
+			
+			if(mockService.getUrl().equals(path) && 
+					"POST".equals(mockService.getHttpMethod())) {
+				
+				return Response.ok(mockService.getResponse()).build();
+			}
+			
+		}
+		return Response.ok().build();
+	}
+	
+	@POST
+	@Path("/mock_service")
+	public Response postClient(MockService mockservice)
+	{
+		mockServiceList.add(mockservice);
+//		ResponseBuilder builder = Response.status(Status.CREATED);
+		return Response.ok(mockservice).header("Access-Control-Allow-Origin", "POST").build();
+	}
+	
+	
+	
+//	@PUT
+//	@Path("/{put}")
+//	public Response putClient(@PathParam("value") String value)
+//	{
+//		String message = "Emrah Öztürk";
+//		//return Response.ok(message).build();
+//		return Response.ok().header("Access-Control-Allow-Origin", "*").build();
+//	}
+	
+}
---
+++ src/main/java/com/tai/service/MockService.java
@@ -0,0 +1,33 @@
+package com.tai.service;
+
+public class MockService {
+//	MockService
+	private String url;
+	private String httpMethod;
+	private String response;
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getHttpMethod() {
+		return httpMethod;
+	}
+
+	public void setHttpMethod(String httpMethod) {
+		this.httpMethod = httpMethod;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+}
